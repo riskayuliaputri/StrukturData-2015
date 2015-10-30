78af7d59a300d1090a4fb2581bb596301e2fd52b
riska yulia putri <riskayuliaputri12@gmail.com>
2015-10-30 16:34:46 +0700

    code riska yp

---
+++ 01-Vector/Vector3D.java
@@ -0,0 +1,31 @@
+public class Vector3D
+{
+    
+    double x,y,z;
+    
+    public double x()
+    {
+        // initialise instance variables
+        return x;
+    }
+    
+    public double y()
+    {
+        // initialise instance variables
+        return y;
+    }
+    
+    public double z()
+    {
+        // initialise instance variables
+        return z ;
+    }
+    
+    public Vector3D (double x, double y, double z){
+        this.x=x;
+        this.y=y;
+        this.z=z;
+    
+    }
+
+}
