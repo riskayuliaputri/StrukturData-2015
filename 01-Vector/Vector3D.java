7c1a48cde6ffeecd5329f95a7661d7e9caa63463
riska yulia putri <riskayuliaputri12@gmail.com>
2015-10-30 17:27:43 +0700

    code baru

--- 01-Vector/Vector3D.java
+++ 01-Vector/Vector3D.java
@@ -1,31 +1,45 @@
 public class Vector3D
 {
+    double x,y,z; 
+     
+     
     
-    double x,y,z;
-    
-    public double x()
+  private double x()
     {
         // initialise instance variables
         return x;
     }
     
-    public double y()
+   private double y()
     {
         // initialise instance variables
         return y;
     }
     
-    public double z()
+    private double z()
     {
         // initialise instance variables
         return z ;
     }
     
-    public Vector3D (double x, double y, double z){
-        this.x=x;
-        this.y=y;
-        this.z=z;
     
+    public Vector3D (double x, double y, double z){
+       this.x=x;
+       this.y=y;
+       this.z=z;     
     }
+    
+    public double getX(){
+        return x;
+    }
+    
+    public double getY(){
+        return y;
+    }
+    
+    public double getZ(){
+        return z;
+    }
+    
 
 }
