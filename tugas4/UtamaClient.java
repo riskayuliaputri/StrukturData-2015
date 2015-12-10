bb51452a1a854bacbf4414c9bb675716d22193e1
riska yulia putri <riskayuliaputri12@gmail.com>
2015-12-10 20:45:27 +0700

    tugas

---
+++ tugas4/UtamaClient.java
@@ -0,0 +1,10 @@
+import java.io.IOException;
+
+import java.net.UnknownHostException;
+
+public class UtamaClient {
+    public static void main(String[] args) throws UnknownHostException, IOException {
+        Client client = new Client();
+        client.chat();
+    }
+}
